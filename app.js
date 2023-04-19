ae0a390c2add3e998e32a7ca47ffdf70746cd2a6
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-04-19 21:39:10 +0200

    ♻️ Update : Add and use new CORS Package

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 require('./helpers/connect_mongoose');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
@@ -14,29 +15,31 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Setting Header
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization '
+//   );
+
+//   if (req.method === 'OPTIONS') {
+//     res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE');
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+// Enable Cors
+app.use(cors());
+// Routes Handler
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization '
-  );
-
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE');
-    return res.status(200).json({});
-  }
-  next();
-});
 
 app.use((req, res, next) => {
   const error = new Error('Not Found');
   error.status = 404;
   next(error);
 });
-
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
