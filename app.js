269f1d158ab8b69ead24647e0aa0cc914e1a9071
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-04-30 17:57:46 +0200

    ♻️ Update : app js file

--- app.js
+++ app.js
@@ -5,29 +5,32 @@ const http = require('http');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 require('./helpers/connect_mongoose');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.Promise = global.Promise;
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization '
-  );
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization '
+//   );
 
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE');
-    return res.status(200).json({});
-  }
-});
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE');
+//     return res.status(200).json({});
+//   }
+// });
 
 app.use((req, res, next) => {
   const error = new Error('Not Found');
@@ -36,7 +39,7 @@ app.use((req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
-const server = http.createServer();
+const server = http.createServer(app);
 server.listen(port);
 
 module.exports = app;
