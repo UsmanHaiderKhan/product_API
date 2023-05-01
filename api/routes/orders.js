d4167399c42be76af8f02c601d720fdaca107054
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-05-01 17:55:40 +0200

    🎉 Add : order route file add all the end points

---
+++ api/routes/orders.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders GET Request',
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId,
+  });
+  order
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+  const id = req.params.orderId;
+  if (id === 'special') {
+    res.status(200).json({
+      message: 'special case is detected',
+    });
+  } else {
+    res.status(200).json({
+      message: 'Orders With ID',
+    });
+  }
+});
+
+router.patch('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Update Orders',
+    orderId: req.params.orderId,
+  });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Delete Orders',
+    orderId: req.params.orderId,
+  });
+});
+
+module.exports = router;
