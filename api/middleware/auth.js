6d7a0bb91fb53ae53e46a3c51f63031c1926b333
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-05-07 21:35:19 +0200

    ♻️ Update : replace res ==> req and add new msg

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -3,11 +3,11 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_KEY);
-    res.userData = decoded;
+    req.userData = decoded;
     next();
   } catch (error) {
     return res.status(401).json({
-      message: 'Auth Failed',
+      message: 'Auth Failed Token is not defined.',
     });
   }
 };
