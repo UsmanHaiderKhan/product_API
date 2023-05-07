7a11fdba1daf101c18a2e6a165160f11f91da0d7
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-05-08 00:07:24 +0200

    🎉🎁 Add : New file for auth controller

---
+++ api/Controllers/auth.controller.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Auth = require('../models/auth.model');
+
+module.exports = {
+  // POST : Create New User
+
+  createNewUser: (req, res, next) => {
+    Auth.find({ email: req.body.email })
+      .exec()
+      .then((auth) => {
+        if (auth.length >= 1) {
+          return res.status(409).json({
+            message: 'Email already Exist!!!',
+          });
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) {
+              return res.status(500).json({
+                error: err,
+              });
+            } else {
+              const auth = new Auth({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash,
+              });
+              auth
+                .save()
+                .then((result) => {
+                  res.status(201).json({
+                    message: 'User Has Been Created Successfully',
+                  });
+                })
+                .catch((err) => {
+                  console.log(err);
+                  res.status(500).json({
+                    error: err.message,
+                  });
+                });
+            }
+          });
+        }
+      });
+  },
+
+  // POST : Login
+
+  loggedIn: (req, res, next) => {
+    Auth.find({ email: req.body.email })
+      .exec()
+      .then((auth) => {
+        if (auth.length < 1) {
+          return res.status(401).json({
+            message: 'Login Authorization Failed',
+          });
+        } else {
+          bcrypt.compare(req.body.password, auth[0].password, (err, result) => {
+            if (err) {
+              return res.status(401).json({
+                message: 'Authorization Failed',
+              });
+            }
+            if (result) {
+              const token = jwt.sign(
+                {
+                  email: auth[0].email,
+                  userId: auth[0]._id,
+                },
+                process.env.JWT_KEY,
+                {
+                  expiresIn: '1h',
+                }
+              );
+              return res.status(200).json({
+                message: 'Token Authorization Successful',
+                token: token,
+              });
+            }
+            res.status(401).json({
+              message: 'Token Authorization Failed',
+            });
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err.message,
+        });
+      });
+  },
+
+  // DELETE : User By Id
+
+  deleteUser: (req, res, next) => {
+    Auth.findByIdAndDelete({ _id: req.params.userId })
+      .exec()
+      .then((result) => {
+        res.status(200).json({
+          message: 'User Has Been Deleted Successfully',
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err.message,
+        });
+      });
+  },
+};
